8b2e725cc9c53ab840a9dbd633893f79b2865623
Abhishek <abhishekajmera0903@gmail.com>
2020-08-12 10:03:04 +0530

    Updates

---
+++ precache-manifest.3830be880e8826a42f319c3e503e29a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6eda2e0128779d1b8d478c4926b10a3",
+    "url": "/Frontend-UNICODE.git/index.html"
+  },
+  {
+    "revision": "c465dbbe782fde240513",
+    "url": "/Frontend-UNICODE.git/static/css/main.8b8c2d44.chunk.css"
+  },
+  {
+    "revision": "c1ecbd37f7c15fa991b5",
+    "url": "/Frontend-UNICODE.git/static/js/2.6d0c2279.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Frontend-UNICODE.git/static/js/2.6d0c2279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c465dbbe782fde240513",
+    "url": "/Frontend-UNICODE.git/static/js/main.8f506498.chunk.js"
+  },
+  {
+    "revision": "ec874104efa3eb1f1210",
+    "url": "/Frontend-UNICODE.git/static/js/runtime-main.49090b7f.js"
+  }
+]);
